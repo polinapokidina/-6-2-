17cacf32172979fc27b52e75f5393b2c7763e224
user <user@LAPTOP-VCVPT2J9>
2023-04-21 15:17:13 +0300

    Добавьте файлы проекта.

---
+++ Lab6_2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Lab6_2
+{
+    partial class Lab7
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecision = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 206);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Вычислить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(15, 155);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Последовательность: x + x/2 + x/3 + x/4  + ...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "x =";
+            // 
+            // txtX
+            // 
+            this.txtX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtX.Location = new System.Drawing.Point(42, 86);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(48, 20);
+            this.txtX.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "точность =";
+            // 
+            // txtPrecision
+            // 
+            this.txtPrecision.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPrecision.Location = new System.Drawing.Point(216, 86);
+            this.txtPrecision.Name = "txtPrecision";
+            this.txtPrecision.Size = new System.Drawing.Size(71, 20);
+            this.txtPrecision.TabIndex = 6;
+            // 
+            // Lab7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 252);
+            this.Controls.Add(this.txtPrecision);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Lab7";
+            this.Text = "Сумма ряда";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrecision;
+    }
+}
+
---
+++ Lab6_2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6_2
+{
+    public partial class Lab7 : Form
+    {
+        public Lab7()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int counter = 0; //Количество слагаемых
+            double sum = 0; //Сумма
+            double summand = 0.0; //слагаемое
+            double x;
+            bool f = false;
+            double precision;
+
+            if (double.TryParse(txtPrecision.Text, out precision))
+            {
+                f = true;
+            }
+
+
+            do
+            {
+                counter++;
+                if (double.TryParse(txtX.Text, out x) )
+                {
+                    summand = double.Parse(txtX.Text) / counter;
+                }
+
+                sum += summand;
+                
+            }   while (f && Math.Abs(summand) > precision);
+
+            if (f)
+            {
+                lblResult.Text = "сумма = " + sum + ", количество =" + counter; //Заполнение суммы
+            }
+
+            else
+            {
+                MessageBox.Show("Введите числовое значение");
+            }
+        }
+    }
+}
